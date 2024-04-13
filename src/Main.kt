184ad41c65211b70a3f3cc3b36c215deb6968b79
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2024-04-13 20:40:42 +0530

    updated first kotlin file

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//fun main() {
+//    val name = "Kotlin"
+//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//    // to see how IntelliJ IDEA suggests fixing it.
+//    println("Hello, " + name + "!")
+//
+//    for (i in 1..5) {
+//        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//        println("i = $i")
+//    }
+//}
+fun main(){
+    var name: String = "saket" // var is read and write meaning it can be changed in the future
+    name = "SAKET"
+    var fname = "saket" //type inference is not required always
+    val lname: String = "singh" //val is read only and hence it cannot be reassigned
+//    lname = "SINGH"
+    println("hello $name  $lname")
+    println("hello $fname ")
 }
---
+++ src/types.kt
@@ -0,0 +1,25 @@
+fun main() {
+    var number: Int = 20
+    var name = "Saket"
+    val maxIntegerValue: Int = Int.MAX_VALUE
+    val minIntegerValue: Int = Int.MIN_VALUE
+
+    println("maxIntegerValue = $maxIntegerValue\nmaxIntegerValue = $minIntegerValue")
+
+    val maxByteValue: Byte = Byte.MAX_VALUE
+    val minByteValue: Byte = Byte.MIN_VALUE
+    println("maxByteValue = $maxByteValue\nminByteValue = $minByteValue")
+    val maxShortValue: Short = Short.MAX_VALUE
+    val minShortValue: Short = Short.MIN_VALUE
+    println("maxShortValue = $maxShortValue\nminShortValue = $minShortValue")
+    val maxLongValue: Long = Long.MAX_VALUE
+    val minLongValue: Long = Long.MIN_VALUE
+    println("maxLongValue = $maxLongValue\nminLongValue = $minLongValue")
+    var no1 = 2F //float
+    var no2 = 2.0
+    println("first no $no1\nsecond no: $no2")
+    var charValue = 'S'
+    var BooleanValue = true // or false
+    println("char = $charValue")
+    println(BooleanValue) // can also directly print 
+}
